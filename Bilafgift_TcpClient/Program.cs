5226ca7d8a4faa1caff2518c7c1211217eb2a19c
Mikail Fener <mika1292@edu.easj.dk>
2019-03-16 23:08:05 +0100

    Add project files.

---
+++ Bilafgift_TcpClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bilafgift_TcpClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
